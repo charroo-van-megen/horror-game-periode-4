6abd9f9fc74f6f85a03b1454f666fb0e0f59062c
charroo <charroovanmegen01@gmail.com>
2026-06-09 14:55:34 +0200

    mouselook

--- Assets/Scripts/camera/Mouselook.cs
+++ Assets/Scripts/camera/Mouselook.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
-public class Mouselook : MonoBehaviour
+public class MouseLook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("References")]
+    public Transform playerBody;
+
+    [Header("Settings")]
+    public float mouseSensitivity = 100f;
+
+    private float xRotation = 0f;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Vertical camera rotation
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Horizontal player rotation
+        playerBody.Rotate(Vector3.up * mouseX);
     }
-}
+    }
